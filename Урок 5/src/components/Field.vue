655ad4494b0b341634d6cec4b06cc67091500e5a
Yuriy Klimenko <yurriyklimenko@gmail.com>
2022-09-18 13:33:03 +0300

    lesson-5 anim fix

--- Урок 5/src/components/Field.vue
+++ Урок 5/src/components/Field.vue
@@ -2,31 +2,46 @@
 	<div class="form-group">
 		<label>
 			{{ label }}
-         <transition-group name="fade" mode="in-out">
-         <template v-if="activated">
-            <fa-icon v-if="valid"
-				class="text-success"
-				icon="check-circle"
-			/>
-         <fa-icon v-if="!valid"
-				class="text-danger"
-				icon="exclamation-circle"
-			/>
-         </template>
-      </transition-group>
+            <transition name="rotate" mode="out-in">
+                  <fa-icon v-if="valid && activated"
+                  class="text-success"
+                  icon="check-circle"
+                  />
+                  <fa-icon v-else-if="!valid && activated"
+                  class="text-danger"
+                  icon="exclamation-circle"
+                  />
+         </transition>
 		</label>
 		<input type="text" class="form-control" :value="value" @input="onInput">
 	</div>
 </template>
 <style>
-.fade-enter-active, .mode-fade-leave-active {
-  transition: opacity .5s ease
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .3s ease;
 }
 
-.fade-enter-from, .mode-fade-leave-to {
-  opacity: 0
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 
+.rotate-enter-active {
+  animation: rotate .25s;
+}
+.rotate-leave-active {
+  animation: rotate .25s reverse;
+}
+@keyframes rotate {
+  0% {
+    transform: rotateY(90deg);
+  }
+  100% {
+   transform: rotateY(0deg);
+  }
+}
 </style>
 <script>
    
